4e87885ddaa1c49a31d81968b5b3354d91e015af
Yanir <yanir401@gmail.com>
2022-11-08 23:23:17 +0200

    improve redux and add toggle if user logged in

--- client/src/components/layout/navigation/Navigation.jsx
+++ client/src/components/layout/navigation/Navigation.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./navigation.css";
 const Navigation = () => {
+  const { user } = useSelector((state) => state.user);
+
+  // const authenticate = () => {
+  //   return user ? <Link to="profile">Profile</Link> : <a> Sign in</a>;
+  // };
+  const authenticate = (
+    <>{user ? <Link to="profile">Profile</Link> : <a> Sign in</a>}</>
+  );
+
   return (
     <div className="navbar">
       <h2>Logo</h2>
@@ -13,9 +23,7 @@ const Navigation = () => {
           <li>
             <Link to="events">Events</Link>
           </li>
-          <li>
-            <Link>Profile</Link>
-          </li>
+          <li>{authenticate}</li>
         </ul>
       </nav>
     </div>
