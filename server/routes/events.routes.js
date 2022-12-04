c50f8a152c5cf77a222a01e31262adc4d3cc3cef
Yanir <yanir401@gmail.com>
2022-12-05 00:13:42 +0200

    import problem resolved

--- server/routes/events.routes.js
+++ server/routes/events.routes.js
@@ -18,18 +18,22 @@ eventsRouter.get("/", getAllEvents);
 eventsRouter.get("/upcoming-events", getUpComingEvents);
 
 //events created by the user
-eventsRouter.get("/my-events", authMiddleware, getUserEvents);
+eventsRouter.get("/my-events", getUserEvents);
+// eventsRouter.get("/my-events", authMiddleware, getUserEvents);
 
 eventsRouter.get("/event/:id", getEvent);
 
 //users attends events
 // eventsRouter.get("/attend-events", getUserEvents);
 
-eventsRouter.post("/", authMiddleware, createEvent);
+eventsRouter.post("/", createEvent);
+// eventsRouter.post("/", authMiddleware, createEvent);
 
 // eventsRouter.patch("/", updateEvent);
 
 eventsRouter.delete("/:id", deleteEvent);
 
-eventsRouter.patch("/join-volunteering", authMiddleware, joinVolunteering);
-eventsRouter.patch("/leave-volunteering", authMiddleware, leaveVolunteering);
+eventsRouter.patch("/join-volunteering", joinVolunteering);
+// eventsRouter.patch("/join-volunteering", authMiddleware, joinVolunteering);
+// eventsRouter.patch("/leave-volunteering", authMiddleware, leaveVolunteering);
+eventsRouter.patch("/leave-volunteering", leaveVolunteering);
